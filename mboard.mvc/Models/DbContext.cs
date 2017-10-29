7c0f4a825d4615c92c7407c66dc785b4156f486a
= <piotr_nowakowski@hotmail.com>
2017-10-29 12:41:58 +0100

    init commit

---
+++ mboard.mvc/Models/DbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Neo4j.Driver.V1;
+
+namespace mboard.mvc.Models
+{
+  public class NeoDbContext
+  {
+    public string  Name { get; set; }
+  }
+}
